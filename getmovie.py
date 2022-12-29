b969972255f8f9061ea54a22c2d8cd22bec3c31f
William Renaldy <williamrenaldy.a@gmail.com>
2022-12-29 21:05:02 +0530

    Update getmovie.py

--- getmovie.py
+++ getmovie.py
@@ -9,13 +9,35 @@ from tqdm import tqdm
 import warnings
 import chromedriver_autoinstaller
 
+
+import requests
+import wget
+import zipfile
+import os
+
+# get the latest chrome driver version number
+url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+response = requests.get(url)
+version_number = response.text
+
+# build the donwload url
+download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
+
+# download the zip file using the url built above
+latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+
+# extract the zip file
+with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+    zip_ref.extractall() # you can specify the destination folder path here
+# delete the zip file downloaded above
+os.remove(latest_driver_zip)
+
 warnings.filterwarnings("ignore")
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 options.add_argument("--headless")
 
-chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome(options=options)
 
