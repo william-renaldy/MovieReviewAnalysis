ec4b8ad83b86da133bf55e9537402bc91334fd14
William Renaldy <williamrenaldy.a@gmail.com>
2022-12-29 21:37:22 +0530

    Update getmovie.py

--- getmovie.py
+++ getmovie.py
@@ -10,30 +10,30 @@ import warnings
 import chromedriver_autoinstaller
 
 
-import requests
-import wget
-import zipfile
-import os
+# import requests
+# import wget
+# import zipfile
+# import os
 
-# get the latest chrome driver version number
-url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
-response = requests.get(url)
-version_number = response.text
+# # get the latest chrome driver version number
+# url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+# response = requests.get(url)
+# version_number = response.text
 
-# build the donwload url
-download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
+# # build the donwload url
+# download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
 
-# download the zip file using the url built above
-latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+# # download the zip file using the url built above
+# latest_driver_zip = wget.download(download_url,'chromedriver.zip')
 
-# extract the zip file
-with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
-    zip_ref.extractall("driver") # you can specify the destination folder path here
-# delete the zip file downloaded above
-#os.remove(latest_driver_zip)
+# # extract the zip file
+# with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+#     zip_ref.extractall("driver") # you can specify the destination folder path here
+# # delete the zip file downloaded above
+# #os.remove(latest_driver_zip)
 
 
-os.environ["PATH"] = "/driver/chromedriver.exe"
+# os.environ["PATH"] = "/driver/chromedriver.exe"
 
 warnings.filterwarnings("ignore")
 
@@ -42,7 +42,7 @@ options.add_argument("--start-maximized")
 options.add_argument("--headless")
 
 
-driver = webdriver.Chrome(executable_path="driver/chromedriver.exe",options=options)
+driver = webdriver.Chrome(executable_path=r"driver\chromedriver.exe",options=options)
 
 
 def GetMovie(name)->list:
